1a87efdba3a0a31ff34650bc5af70146271f6dc3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-18 02:50:19 +0100

    :rocket:

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Bonjour</h1>
+    <div className="formulaire-container">
+    {/* Première ligne */}
+    <div className="formulaire-row">
+      <p className="formulaire-label">Email:</p>
+      <input type="text" placeholder="Saisir votre email" />
     </div>
+
+    {/* Deuxième ligne */}
+    <div className="formulaire-row">
+      <p className="formulaire-label">Mot de passe:</p>
+      <input type="password" placeholder="Saisir votre mot de passe" />
+    </div>
+  </div>
   );
 }
 
