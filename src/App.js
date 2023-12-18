d6bb2c22d66e1e24db2c234da4de6a8a3b415527
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-18 04:11:52 +0100

    :rocket:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() =>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((json) =>setUsers(json));
+  })
   return (
     <div className="App">
       <h1>Users</h1>
