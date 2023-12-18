0991b719dccf134db8dabc619596690bec7b8730
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-18 20:09:01 +0100

    :rocket:

--- src/Components/StateReact.js
+++ src/Components/StateReact.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const StateReact = () => {
+    const [count, setCount] = useState(0)
+    const increment = () =>{
+        setCount(count+1)
+    }
+
+
+
   return (
-    <div>StateReact</div>
+    <div className='App'>
+        <h1>{count}</h1>
+        <button onClick={increment}>increment the button</button>
+    </div>
   )
 }
