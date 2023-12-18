071d8d0838edea1868a1fd8334a80d187ec397e4
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-18 20:34:07 +0100

    :rocket:

--- src/Components/StateReact.js
+++ src/Components/StateReact.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react'
 
 export const StateReact = () => {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+    const [isVisible, setIsVisible] = useState(false)
+
     const increment = () =>{
         setCount(count+1)
+        setIsVisible(true)
     }
 
 
@@ -11,6 +15,11 @@ export const StateReact = () => {
   return (
     <div className='App'>
         <h1>{count}</h1>
+        {isVisible ? (
+            <h1>Visible</h1>
+        ) : (
+            <h1>Not visible </h1>
+        )}
         <button onClick={increment}>increment the button</button>
     </div>
   )
