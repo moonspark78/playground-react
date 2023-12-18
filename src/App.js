f7ae0ce7649c27ddfc1d956dd17b53e0a2cb7aed
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-18 09:46:44 +0100

    :rocket:

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [users, setUsers] = useState([])
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
   useEffect(() =>{
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -12,26 +13,36 @@ function App() {
   }, []);
 
   const mapData = () => {
-    let filteredData = users.filter((user) => {
+    let square = numbers.map((number) => {
+        return number * number;
+    });
+    console.log(square);
+   /*  let filteredData = users.filter((user) => {
       return user.id <= 5;
     });
 
-    setUsers(filteredData);
+    setUsers(filteredData); */
   };
 
 
   return (
     <div className="App">
-      <h1>Users</h1>
-      <div className='card'>
+      <h1>Numbers</h1>
+     {/*  <div className='card'>
       {users.map((user) =>(
         <div className='card-inner'>
           <p>{user.name}</p>
           <p>{user.username}</p>
         </div>
       ))}
-        
-      </div>
+      </div> */}
+
+        <div  className='card'>
+          {numbers.map((n) =>(
+            <div className='card-inner'>{n}</div>
+          ))}
+        </div>
+
       <button onClick={mapData}>See Mappedd Array</button>
     </div>
   );
