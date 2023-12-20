f8014d73f34a3fdfce2ed9197de9bba4ea591a1d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-20 14:39:06 +0100

    :rocket:

--- src/Components/InlineConditionalExpressions.js
+++ src/Components/InlineConditionalExpressions.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const InlineConditionalExpressions = () => {
+    useEffect(() => {
+        let age = 26;
+        let name = "Alice";
+        if (age > 26){
+            console.log("You are now old");
+        }else if (name === "Alice" && age === 26){
+            console.log("i'm 26 years old");
+            console.log("Alice is my name");
+        }else{
+            console.log("Alice is not my name");
+            console.log("Im less than 26 years old");
+        }
+
+        age > 26 ? console.log("You are now old") : name === "Alice" && age === 26 ? (
+            <> 
+                {console.log("i'm 26 years old")} 
+                {console.log("Alice is my name")} 
+            </>  
+        ) :   
+             <> 
+                {console.log("i'm 26 years old")} 
+                {console.log("Alice is my name")} 
+            </>  
+    },[]);
   return (
-    <div>InlineConditionalExpressions</div>
+    <div>
+
+    </div>
   )
 }
