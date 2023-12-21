73a2b82dafd42b41b4f772a03a9f75e72bbcd837
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-21 12:38:19 +0100

    working on forms  :rocket:

--- src/Components/FormsInReact.js
+++ src/Components/FormsInReact.js
@@ -1,28 +1,55 @@
 import React from 'react'
+import "./FormsInReact.css"
 
 export const FormsInReact = () => {
-    const getInputs = () =>{
-
+    const getInputs = (value, name) =>{
+        console.log(name);
     }
 
 
 
 
   return (
-    <>
+    <div className='to'>
         <h1>Forms</h1>
         <div className='form'>
-        <input placeholder='Write your name'/>
-        <input placeholder=''/>
-        <input placeholder=''/>
-        <input placeholder=''/>
+        <input 
+            placeholder='Write your name' 
+            onChange={(e) => getInputs(e.target.value, e.target.name)}
+            name='name'
+            type='text'
+        />
+            
+
+        <input 
+            placeholder='Write your age'
+            onChange={(e) => getInputs(e.target.value, e.target.name)}
+            name='age'
+            type='number'
+        />
+
+        <input 
+            placeholder='Write your Hobbies'
+            onChange={(e) => getInputs(e.target.value, e.target.name)}
+            name="hobbies"
+            type='text'
+        />
+
+        <input 
+            placeholder='Write your date'
+            onChange={(e) => getInputs(e.target.value, e.target.name)}
+            name='date'
+            type='date'
+        />
+
+            
         <button>Submit</button>
 
         
 
 
         </div>
-    </>
+    </div>
    
   )
 }
