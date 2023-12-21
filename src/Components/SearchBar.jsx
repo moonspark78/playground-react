70353999cb7876e9c088cc2e9dc4475de5494752
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-21 15:52:01 +0100

    :rocket:

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SearchBar.css"
+import {FaSearch} from "react-icons/fa"
 
 export const SearchBar = () => {
+    const [input, setInput] = useState("")
+
+    const fetchData = () =>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+    };
+
+    const handleChange = (value) =>{
+
+    };
+
+
+
+    
   return (
-    <div>SearchBar</div>
+    <div className='input-wrapper'>
+        <FaSearch id='search-icon'/>
+        <input 
+            placeholder='Type to Search ...' 
+            value={input} 
+            onChange={(e) => setInput(e.target.value)}
+        />
+    </div>
   )
 }
