7e513deb9183c5970d4d543aa30a2c1621a565ee
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-24 22:25:21 +0100

    :art: React Interview  :rocket:

--- src/Components/DynamicInputs.js
+++ src/Components/DynamicInputs.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export const DynamicInputs = () => {
+    const [inputs, setInputs]= useState({});
+    const getInputValue = (data) =>{
+        let {name, value} = data.target;
+        let input = {[name]: value};
+        setInputs(...inputs, ...input);
+    };
+
+
+
+
+
   return (
     <div className='App'>
         <h1>Dynamic Inputs</h1>
