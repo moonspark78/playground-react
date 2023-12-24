ddad9f4230a6b7bbd61fd2b5a29ec5d3b76f3fcd
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 00:40:49 +0100

    :art: Create Uncontrolled Components  :rocket:

--- src/Components/UncontrolledComponents.js
+++ src/Components/UncontrolledComponents.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { createRef } from 'react'
 
 export const UncontrolledComponents = () => {
+    let name = React.createRef();
+    let age = React.createRef();
+
+    const Submit = () =>{
+        console.log(name.current.value);
+        console.log(age.current.value);
+    }
+
   return (
-    <div>
-        <h1 style={{textDecoration : "underline", color: "red"}}>Uncontrolled Components</h1>
+    <div className='cc'>
+        <h1 style={{textDecoration : "underline", color: "red", fontFamily: "cursive"}}>Uncontrolled Components</h1>
+        <hr/>
+
+        <input 
+            placeholder='Name...'
+            ref={name}
+            
+        />
+        <input 
+            placeholder='Age...'
+            ref={age}
+            
+        />
+        <button onClick={Submit} style={{borderRadius: "5px", marginLeft: "18px"}}>Submit</button>
     </div>
   )
 }
