5dc727206d7770dbc6e15982161781017fb04294
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-24 20:40:25 +0100

    :art: React Interview  :rocket:

--- src/Components/DynamicInputs.js
+++ src/Components/DynamicInputs.js
@@ -6,9 +6,24 @@ export const DynamicInputs = () => {
     <div className='App'>
         <h1>Dynamic Inputs</h1>
         <div className='input-container'>
-            <input placeholder='Name'/>
-            <input placeholder='Age'/>
-            <input placeholder='Years of Experience'/>
+            <input 
+                placeholder='Name' 
+                name='name'
+                onChange={getInputValue}
+
+                />
+            <input 
+                placeholder='Age' 
+                name='age'
+                onChange={getInputValue}
+
+                />
+            <input 
+                placeholder='Years of Experience' 
+                name='yoe'
+                onChange={getInputValue}
+
+                />
         </div>
         <button>Add New Group</button>
     </div>
