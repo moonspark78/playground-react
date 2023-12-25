77c871dac3e6f9b24d97696a0da3cf65c7519cf4
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 20:59:42 +0100

    :art: RestAPIComponent  :rocket:

--- src/Components/RestAPI.js
+++ src/Components/RestAPI.js
@@ -7,7 +7,8 @@ import axios from 'axios'
 
 
 export const RestAPI = () => {
-    const [name, setName] = useState("")
+    const [name, setName] = useState("");
+    const [users, setUsers] = useState([]);
     // Post, Get, Put, Delete
     const postData = () =>{
         axios
@@ -24,11 +25,32 @@ export const RestAPI = () => {
         })
     };
 
+    const updateData = (id) =>{
+         console.log(id);
+         axios
+         .put("https://65899f2f324d4171525942ec.mockapi.io/users", {
+            name: name,
+            age: 56,
+            hobbies: ["Boxing", "Cooking", "Soccer", "Coding", "Piano"],
+         }, id)
+         .then((res) =>{
+            console.log(res.data);
+        })
+        .catch((err) =>{
+            console.log(err);
+        })
+    };
+
+
+
+
+
+
     useEffect(() => {
         axios
         .get("https://65899f2f324d4171525942ec.mockapi.io/users", )
         .then((res) =>{
-            console.log(res.data);
+            setUsers(res.data);
         })
         .catch((err) =>{
             console.log(err);
@@ -47,6 +69,16 @@ export const RestAPI = () => {
          <h1 style={{textDecoration : "underline", color: "red", fontFamily: "inherit"}}>RestAPI</h1><br/><br/><br/>
         <input style={{border: "solid 1px black", width:"200px"}} placeholder='Name' onChange={(e) => setName(e.target.value)}/>
         <button onClick={postData} style={{borderRadius: "5px", width: "90px", height: "30px"}}>POST Data</button>
+        {
+            users.map((user) =>{
+                return (
+                    <>
+                        <h3>{`${user.id}. ${user.name}`}</h3>
+                        <button onClick={() => updateData(user.id)} style={{borderRadius: "5px", width: "90px", height: "30px", color: "white"}}>Update</button>
+                    </>
+                )
+            })
+        }
     </div>
   )
 }
