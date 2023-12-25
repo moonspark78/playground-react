0b4f96664a0bca29cc602173b88d0bcc525d2bed
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 12:14:31 +0100

    :art: Working... on InnerHTML   :rocket:

--- src/Components/InnerHTML.js
+++ src/Components/InnerHTML.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export const InnerHTML = () => {
+    const [quill, setQuill] = useState("");
+
     let data = `<p style="font-size: 30px">This is my. Home</p>`
     let data2 = `<b style="font-size: 30px">This is my. Home</b>`
 
-
+    const getQuillData = (value) =>{
+        setQuill(value);
+    };
 
 
 
@@ -16,6 +22,8 @@ export const InnerHTML = () => {
         <div className='App'>
             <div dangerouslySetInnerHTML={{__html: data}}></div>
             <div dangerouslySetInnerHTML={{__html: data2}}></div>
+
+            <br/><br/><br/><ReactQuill onChange={getQuillData}/>
         </div>
     </>
   )
