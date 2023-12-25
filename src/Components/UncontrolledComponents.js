d5914232a05cc9ce95d8c2de945fe1287133a08c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 02:59:31 +0100

    :art: UncontrolledComponents VS ControlledComponents   :rocket:

--- src/Components/UncontrolledComponents.js
+++ src/Components/UncontrolledComponents.js
@@ -4,6 +4,8 @@ export const UncontrolledComponents = () => {
     let name = React.createRef();
     let age = React.createRef();
 
+
+    
     const Submit = () =>{
         console.log(name.current.value);
         console.log(age.current.value);
@@ -14,16 +16,15 @@ export const UncontrolledComponents = () => {
         <h1 style={{textDecoration : "underline", color: "red", fontFamily: "cursive"}}>Uncontrolled Components</h1>
         <hr/>
 
-        <input 
-            placeholder='Name...'
-            ref={name}
-            
-        />
-        <input 
-            placeholder='Age...'
-            ref={age}
-            
-        />
+
+        {/* Uncontrolled Component */}
+        <input placeholder='Name...' ref={name} />
+        <input  placeholder='Age...' ref={age} />
+
+        {/* Controlled Component */}
+        <input placeholder='Name...' ref={name} />
+        <input  placeholder='Age...' ref={age} />
+        
         <button onClick={Submit} style={{borderRadius: "5px", marginLeft: "18px"}}>Submit</button>
     </div>
   )
