3a941d481efdf9d34266a7d9174f480bbabe67ff
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 18:01:45 +0100

    :art: RestAPIComponent  :rocket:

--- src/Components/RestAPI.js
+++ src/Components/RestAPI.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 
@@ -7,12 +7,20 @@ import axios from 'axios'
 
 
 export const RestAPI = () => {
+    const [name, setName] = useState("")
     // Post, Get, Put, Delete
     const postData = () =>{
-        axios.post("https://65899f2f324d4171525942ec.mockapi.io/users", {
-            name: "Alice",
+        axios
+        .post("https://65899f2f324d4171525942ec.mockapi.io/users", {
+            name: name,
             age: 23,
             hobbies: ["Poetry", "Cooking", "Travelling", "Coding", "Piano"],
+        })
+        .then((res) =>{
+            console.log(res.data);
+        })
+        .catch((err) =>{
+            console.log(err);
         })
     };
 
@@ -26,7 +34,8 @@ export const RestAPI = () => {
   return (
     <div>
          <h1 style={{textDecoration : "underline", color: "red", fontFamily: "inherit"}}>RestAPI</h1><br/><br/><br/>
-        <button onClick={postData} style={{borderRadius: "5px", width: "90px"}}>POST Data</button>
+        <input style={{border: "solid 1px black", width:"200px"}} placeholder='Name' onChange={(e) => setName(e.target.value)}/>
+        <button onClick={postData} style={{borderRadius: "5px", width: "90px", height: "30px"}}>POST Data</button>
     </div>
   )
 }
