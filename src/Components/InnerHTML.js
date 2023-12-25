3f767ce238dfdaa5e964436ad3a0831958d072ff
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 11:29:16 +0100

    :art: Working... on InnerHTML   :rocket:

--- src/Components/InnerHTML.js
+++ src/Components/InnerHTML.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const InnerHTML = () => {
-    let data = `<p>This is my. Home</p>`
+    let data = `<p style="font-size: 30px">This is my. Home</p>`
 
 
 
@@ -13,7 +13,7 @@ export const InnerHTML = () => {
     <>
         <h1 style={{textDecoration : "underline", color: "red", fontFamily: "inherit"}}>InnerHTML</h1>
         <div className='App'>
-        {data}
+            <div dangerouslySetInnerHTML={{__html: data}}></div>
         </div>
     </>
   )
