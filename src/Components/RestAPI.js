113366cd4fb0a2bae0a6766bafcec7ae15885369
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-25 16:45:21 +0100

    :art: RestAPIComponent  :rocket:

--- src/Components/RestAPI.js
+++ src/Components/RestAPI.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+
+
+
+
+
 
 export const RestAPI = () => {
+    // Post, Get, Put, Delete
+    const postData = () =>{
+        axios.post("https://65899f2f324d4171525942ec.mockapi.io/users", {
+            name: "Alice",
+            age: 23,
+            hobbies: ["Poetry", "Cooking", "Travelling", "Coding", "Piano"],
+        })
+    };
+
+
+
+
+
+
+
+
   return (
-    <div>RestAPI</div>
+    <div>
+         <h1 style={{textDecoration : "underline", color: "red", fontFamily: "inherit"}}>RestAPI</h1><br/><br/><br/>
+        <button onClick={postData} style={{borderRadius: "5px", width: "90px"}}>POST Data</button>
+    </div>
   )
 }
