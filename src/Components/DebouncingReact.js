3e4b3aad68ccf20053bf8cc5fd6d2c41aad75be9
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-26 16:54:00 +0100

    :art: DebouncingReact Components  :rocket:

--- src/Components/DebouncingReact.js
+++ src/Components/DebouncingReact.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import axios from 'axios';
+
+
+
+
+const pinAPI = `https://api.postalpincode.in/pincode/`;
+
 
 export const DebouncingReact = () => {
+    const searchPin = (value) =>{
+        axios.get(pinAPI + value).then((response) =>{
+          console.log(response);
+        })
+    };
+
+
+
+
+
+
+
+
   return (
-    <div>DebouncingReact</div>
+    <>
+        <h1 style={{textDecoration : "underline", color: "red", fontFamily: "cursive"}}>Debouncing React</h1>
+        <div>
+            <input onChange={(e) => searchPin(e.target.value)} placeholder='Enter your Pin-Code' style={{border: "solid 1px black", width: "190px"}}/>
+        </div>
+    </>
   )
 }
